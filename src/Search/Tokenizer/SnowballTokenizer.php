d22fce247bd520fe71e4b0fb75f3290b48ee0904
markusos <markus.ostberg@gmail.com>
2015-03-15 23:18:20 -0400

    Refactored SnowballTokenizer

---
+++ src/Search/Tokenizer/SnowballTokenizer.php
@@ -0,0 +1,28 @@
+<?php namespace Search\Tokenizer;
+
+class SnowballTokenizer extends StemTokenizer {
+
+    private $language;
+
+    function __construct($language = 'english', $stopWords = []) {
+        $this->language = $language;
+        $this->stopWords = $stopWords;
+    }
+
+    /**
+     * Stem all tokens in the input array
+     * @param $tokens array of tokens to stem
+     * @return array Array of the stemmed tokens
+     */
+    protected function stem(array $tokens) {
+        $stem = 'stem_' . $this->language;
+
+        return array_map(function($token) use ($stem) {
+            if (in_array($token, $this->stopWords)) {
+                return $token;
+            }
+            return $stem($token);
+
+        }, $tokens);
+    }
+}
