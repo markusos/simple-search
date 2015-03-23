1993c01bbe4f8141fe6e9205852ca41b78714f8a
markusos <markus.ostberg@gmail.com>
2015-03-22 20:47:29 -0400

    Break out Engine setup for demo code

---
+++ src/Demo/setup.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+$pdo = new \PDO('sqlite:documents.sqlite3');
+$pdo->setAttribute (\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+$tokenizer = new Search\Tokenizer\PorterTokenizer();
+$store = new Search\Store\SQLDocumentStore($pdo, $tokenizer);
+$index = new Search\Index\MemcachedDocumentIndex();
+$ranker = new Search\Ranker\TFIDFDocumentRanker();
+
+$config = Search\Config\Config::createBuilder()
+                                ->index($index)
+                                ->store($store)
+                                ->tokenizer($tokenizer)
+                                ->ranker($ranker)
+                                ->build();
+
+$engine = new Search\Engine($config);
